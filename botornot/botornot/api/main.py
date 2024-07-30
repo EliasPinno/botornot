1c4014668d7dec3d4ef0378ecad24750ee6bd2e6
EliasPinno <pucas946@gmail.com>
2024-07-29 21:25:48 -0700

    Setting up the pre-commit hooks.

--- botornot/botornot/api/main.py
+++ botornot/botornot/api/main.py
@@ -1,6 +1,6 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-import uvicorn
 
 app = FastAPI()
 
@@ -8,6 +8,7 @@ app = FastAPI()
 @app.get("/", status_code=200)
 async def root():
     return RedirectResponse(url="/docs")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
