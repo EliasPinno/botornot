e8600c806fd186405026268f100b05f7d0cdc344
EliasPinno <pucas946@gmail.com>
2024-07-29 20:15:20 -0700

    More inital repo config work.

---
+++ botornot/botornot/api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/", status_code=200)
+async def root():
+    return RedirectResponse(url="/docs")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
